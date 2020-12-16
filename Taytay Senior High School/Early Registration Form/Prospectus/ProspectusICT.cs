b647fc8c7cd032d344d15ff321e446f95a9e07f6
Kenneth Glenn Somera Bamba <dev.eydeechan@gmail.com>
2020-12-16 14:34:47 +0800

    Added Prospectus

---
+++ Taytay Senior High School/Early Registration Form/Prospectus/ProspectusICT.cs
@@ -0,0 +1,22 @@
+﻿#region Imports
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+#endregion
+
+namespace Early_Registration_Form.Prospectus
+{
+    public partial class ProspectusICT : UserControl
+    {
+        public ProspectusICT()
+        {
+            InitializeComponent();
+        }
+    }
+}
