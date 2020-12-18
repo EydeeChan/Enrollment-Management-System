b1bd70bec51db7c43ee348d3b80f9c3b6f143351
Kenneth Glenn Somera Bamba <dev.eydeechan@gmail.com>
2020-12-18 12:40:14 +0800

    Create User Control RegistrationStart

---
+++ Taytay Senior High School/Early Registration Form/Registration/RegistrationStart.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Early_Registration_Form.Registration
+{
+    partial class RegistrationStart
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnNewStudent = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.BtnTransferee = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnNewStudent
+            // 
+            this.BtnNewStudent.Active = false;
+            this.BtnNewStudent.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(164)))), ((int)(((byte)(162)))));
+            this.BtnNewStudent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(115)))), ((int)(((byte)(115)))));
+            this.BtnNewStudent.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnNewStudent.BorderRadius = 0;
+            this.BtnNewStudent.ButtonText = "New Student";
+            this.BtnNewStudent.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnNewStudent.DisabledColor = System.Drawing.Color.Gray;
+            this.BtnNewStudent.Iconcolor = System.Drawing.Color.Transparent;
+            this.BtnNewStudent.Iconimage = null;
+            this.BtnNewStudent.Iconimage_right = null;
+            this.BtnNewStudent.Iconimage_right_Selected = null;
+            this.BtnNewStudent.Iconimage_Selected = null;
+            this.BtnNewStudent.IconMarginLeft = 0;
+            this.BtnNewStudent.IconMarginRight = 0;
+            this.BtnNewStudent.IconRightVisible = true;
+            this.BtnNewStudent.IconRightZoom = 0D;
+            this.BtnNewStudent.IconVisible = true;
+            this.BtnNewStudent.IconZoom = 90D;
+            this.BtnNewStudent.IsTab = false;
+            this.BtnNewStudent.Location = new System.Drawing.Point(296, 185);
+            this.BtnNewStudent.Name = "BtnNewStudent";
+            this.BtnNewStudent.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(115)))), ((int)(((byte)(115)))));
+            this.BtnNewStudent.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(164)))), ((int)(((byte)(162)))));
+            this.BtnNewStudent.OnHoverTextColor = System.Drawing.Color.White;
+            this.BtnNewStudent.selected = false;
+            this.BtnNewStudent.Size = new System.Drawing.Size(275, 75);
+            this.BtnNewStudent.TabIndex = 322;
+            this.BtnNewStudent.Text = "New Student";
+            this.BtnNewStudent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.BtnNewStudent.Textcolor = System.Drawing.Color.White;
+            this.BtnNewStudent.TextFont = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnNewStudent.Click += new System.EventHandler(this.BtnNewStudent_Click);
+            // 
+            // BtnTransferee
+            // 
+            this.BtnTransferee.Active = false;
+            this.BtnTransferee.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(164)))), ((int)(((byte)(162)))));
+            this.BtnTransferee.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(115)))), ((int)(((byte)(115)))));
+            this.BtnTransferee.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnTransferee.BorderRadius = 0;
+            this.BtnTransferee.ButtonText = "Transferee";
+            this.BtnTransferee.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnTransferee.DisabledColor = System.Drawing.Color.Gray;
+            this.BtnTransferee.Iconcolor = System.Drawing.Color.Transparent;
+            this.BtnTransferee.Iconimage = null;
+            this.BtnTransferee.Iconimage_right = null;
+            this.BtnTransferee.Iconimage_right_Selected = null;
+            this.BtnTransferee.Iconimage_Selected = null;
+            this.BtnTransferee.IconMarginLeft = 0;
+            this.BtnTransferee.IconMarginRight = 0;
+            this.BtnTransferee.IconRightVisible = true;
+            this.BtnTransferee.IconRightZoom = 0D;
+            this.BtnTransferee.IconVisible = true;
+            this.BtnTransferee.IconZoom = 90D;
+            this.BtnTransferee.IsTab = false;
+            this.BtnTransferee.Location = new System.Drawing.Point(634, 185);
+            this.BtnTransferee.Name = "BtnTransferee";
+            this.BtnTransferee.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(115)))), ((int)(((byte)(115)))));
+            this.BtnTransferee.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(164)))), ((int)(((byte)(162)))));
+            this.BtnTransferee.OnHoverTextColor = System.Drawing.Color.White;
+            this.BtnTransferee.selected = false;
+            this.BtnTransferee.Size = new System.Drawing.Size(275, 75);
+            this.BtnTransferee.TabIndex = 323;
+            this.BtnTransferee.Text = "Transferee";
+            this.BtnTransferee.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.BtnTransferee.Textcolor = System.Drawing.Color.White;
+            this.BtnTransferee.TextFont = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTransferee.Click += new System.EventHandler(this.BtnTransferee_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.DimGray;
+            this.label13.Location = new System.Drawing.Point(318, 330);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(289, 89);
+            this.label13.TabIndex = 324;
+            this.label13.Text = "You are :";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.AutoSize = true;
+            this.lblstatus.Font = new System.Drawing.Font("Segoe UI", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblstatus.ForeColor = System.Drawing.Color.DimGray;
+            this.lblstatus.Location = new System.Drawing.Point(613, 330);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.Size = new System.Drawing.Size(0, 89);
+            this.lblstatus.TabIndex = 325;
+            this.lblstatus.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // RegistrationStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(254)))), ((int)(((byte)(254)))));
+            this.Controls.Add(this.lblstatus);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.BtnTransferee);
+            this.Controls.Add(this.BtnNewStudent);
+            this.Name = "RegistrationStart";
+            this.Size = new System.Drawing.Size(1234, 498);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuFlatButton BtnNewStudent;
+        private Bunifu.Framework.UI.BunifuFlatButton BtnTransferee;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lblstatus;
+    }
+}
---
+++ Taytay Senior High School/Early Registration Form/Registration/RegistrationStart.cs
@@ -0,0 +1,51 @@
+﻿#region Imports
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+#endregion
+
+namespace Early_Registration_Form.Registration
+{
+    public partial class RegistrationStart : UserControl
+    {
+        public RegistrationStart()
+        {
+            InitializeComponent();
+        }
+        #region Instance
+
+        private static RegistrationStart _instance;
+
+        public static RegistrationStart Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new RegistrationStart();
+                return _instance;
+            }
+        }
+
+        #endregion Instance
+        #region New Student Button
+        private void BtnNewStudent_Click(object sender, EventArgs e)
+        {
+            GlobalVar.EnrollmentStatus = "New Student";
+            lblstatus.Text = "New Student";
+        }
+        #endregion
+        #region Transferee Button
+        private void BtnTransferee_Click(object sender, EventArgs e)
+        {
+            GlobalVar.EnrollmentStatus = "Transferee";
+            lblstatus.Text = "Transferee";
+        }
+        #endregion
+    }
+}
