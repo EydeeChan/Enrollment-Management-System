272a30d2b3f1a1c2b9cb49f3789d3524e645b7ab
Kenneth Glenn Somera Bamba <dev.eydeechan@gmail.com>
2020-12-18 12:39:25 +0800

    Create User Control Prospectus

---
+++ Taytay Senior High School/Early Registration Form/Prospectus/Prospectus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Early_Registration_Form.Prospectus
+{
+    public partial class Prospectus : UserControl
+    {
+        public Prospectus()
+        {
+            InitializeComponent();
+        }
+        #region Instance
+
+        private static Prospectus _instance;
+
+        public static Prospectus Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Prospectus();
+                return _instance;
+            }
+        }
+
+        #endregion Instance
+    }
+}
